391dcbe485a1865c76921669b421c67277289fdf
Mets3D <metssfm@gmail.com>
2019-05-15 00:04:41 +0100

    Fixed bogus reachable() (I mixed up the axes...)

--- Botato/Botato.py
+++ Botato/Botato.py
@@ -17,7 +17,7 @@ from keyboard_input import keyboard
 # RLBot
 from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
-from rlbot.utils.game_state_util import GameState, GameInfoState
+from rlbot.utils.game_state_util import GameState, GameInfoState, BallState, Physics, Vector3
 
 # RLUtilities
 from rlutilities.simulation import Ball, Field, Game, ray
@@ -53,12 +53,13 @@ def reachable(car, location, time_left):
 	# This function should evolve as does Botato, since as he learns new things, the ball will become reachable in more situations!
 	# This could also be called for enemy cars to check if we can reach the ball before they do, but since this function relies on knowing a bot's abilities, that will be very unreliable.
 
-	if(location.z > 100):
+	if(location.z > 200):
 		return False	# :)
 	
 	# TODO: do some really fancy stuff to correctly calculate how fast we can get there. The more accurate this function is, the sooner Botato might be able to go for the ball. Of course as long as we are only hitting ground balls, it doesn't really matter.
 	speed = 1400 if car.boost < 30 else 2300	# Good enough for Botimus, good enough for me.
-	ground_loc = MyVec3(location.x, location.z, 120)
+	
+	ground_loc = MyVec3(location.x, location.y, 120)
 	dist = distance(car.location, ground_loc).size
 	minimum_speed_to_reach = dist / (time_left+0.0000001)
 	return minimum_speed_to_reach < speed
@@ -66,6 +67,9 @@ def reachable(car, location, time_left):
 def find_soonest_reachable(car, prediction):
 	""" Find soonest reachable ball """
 	for ps in prediction.slices:
+		location = ps.physics.location
+		ground_loc = MyVec3(location.x, location.y, 120)
+		dist = distance(car.location, ground_loc).size
 		dt = ps.game_seconds - car.game_seconds
 		is_reachable = reachable(car, ps.physics.location, dt)
 		if(is_reachable):
@@ -219,6 +223,10 @@ class Strat_HitBallTowardsNet2(Strategy):
 		# Change desired speed so that when dt is higher, make it lower, and when it's lower, make it higher??
 		desired_speed = dist / max(0.01, dt)
 		desired_speed = desired_speed * (3-dt)
+		ETA = 2300 / dist	# ETA is the time under which we can get there. For now, using max speed.
+
+		#if(ETA > dt):		# If 
+
 
 		# Let's just say we want to accelerate about 2 seconds before we reach target, and we have a constant acceleration(or average acceleration).
 		average_accel = 1000
@@ -429,6 +437,7 @@ class Botato(BaseAgent):
 
 				self.controller.jump = keyboard.key_down("space")
 				self.controller.boost = keyboard.key_down("enter")
+
 			# Save/Load State
 			if(keyboard.key_down("left ctrl") and keyboard.key_down("s")):
 				self.saved_state = GameState.create_from_gametickpacket(packet)
@@ -444,6 +453,9 @@ class Botato(BaseAgent):
 				self.training = Training(self, "Straight Kickoff")
 			elif(keyboard.key_down("[2]")):
 				self.training = Training(self, "Prediction 1")
+			elif(keyboard.key_down("[3]")):
+				self.training = Training(self, "Random Ball Impulse")
+
 			# Change Game Speed
 			if(keyboard.key_down("left ctrl") and keyboard.key_down("[-]")):
 				#game_info_state = GameInfoState(game_speed=packet.game_info.game_speed-0.1)
--- Botato/Training.py
+++ Botato/Training.py
@@ -13,37 +13,61 @@ class Training():
 		self.car_state = CarState()
 		self.ball_state = BallState()
 		self.game_info_state = GameInfoState(game_speed=1)
+		self.game_state = GameState()
 		self.training_name = training_name
 		self.car = car
-		self.reset()
 		
 		# Default values
-		self.car_loc = Vector3(0, 3000, 18)
-		self.car_rot = gsu_rot(-0.016,0,0)
-		self.car_vel = Vector3(0, 0, 0)
-		self.car_av = Vector3(0,0,0)
-		self.car_boost = 33
+		self.car_loc = None
+		self.car_rot = None
+		self.car_vel = None
+		self.car_av = None
+		self.car_boost = 100
 
-		self.ball_loc = Vector3(0, 0, 93)
-		self.ball_vel = Vector3(0,0,0)
-		self.ball_av = Vector3(0,0,0)
-		
+		self.ball_loc = None
+		self.ball_vel = None
+		self.ball_av = None
+
+		def reset_car():
+			self.car_loc = Vector3(0, 3000, 18)
+			self.car_rot = gsu_rot(-0.016,0,0)
+			self.car_vel = Vector3(0, 0, 0)
+			self.car_av = Vector3(0,0,0)
+			self.car_boost = 33
+
+		def reset_ball():
+			self.ball_loc = Vector3(0, 0, 93)
+			self.ball_vel = Vector3(0,0,0)
+			self.ball_av = Vector3(0,0,0)
 
 		if(training_name == "Diagonal Kickoff"):
+			reset_car()
+			reset_ball()
 			self.car_loc = Vector3(-2047, 2559, 18)
 			self.car_rot = gsu_rot(-0.016, -0.785, 0)
 
 		elif(training_name == "Straight Kickoff"):
+			reset_car()
+			reset_ball()
 			self.car_loc = Vector3(256, 3839, 18)
 			self.car_rot = gsu_rot(-0.016, -1.570, 0)
 		
 		elif(training_name == "Prediction 1"):
+			reset_car()
+			reset_ball()
 			self.ball_loc = Vector3(2300, 0, 93)
 			self.car_rot = gsu_rot(-0.016, -1.570, 0)
 
 		elif(training_name == "Random Ground"):
+			reset_car()
+			reset_ball()
 			self.ball_loc = Vector3(random.random()*arena.x*0.8, random.random()*arena.y*0.8, 93)
 			self.car_loc = Vector3(random.random()*arena.x*0.8, random.random()*arena.y*0.8, 18)
+		
+		elif(training_name == "Random Ball Impulse"):
+			self.ball_vel = Vector3(random.random()*1000000, random.random()*1000000, random.random()*1000000)
+			self.car_loc = None
+			self.ball_loc = None
 		else:
 			print("Invalid training name: " + training_name)
 		
@@ -54,7 +78,8 @@ class Training():
 		if(not training_name):
 			training_name = self.training_name
 		
-		car_state = CarState(physics=Physics(velocity=self.car_vel, rotation=self.car_rot, location=self.car_loc), boost_amount=car_boost)
+		car_state = CarState(physics=Physics(velocity=self.car_vel, rotation=self.car_rot, location=self.car_loc), boost_amount=self.car_boost)
 		ball_state = BallState(Physics(velocity=self.ball_vel, location=self.ball_loc, angular_velocity=self.ball_av))
-		game_state = GameState(ball=self.ball_state, cars={self.car.index: self.car_state})
-		self.car.set_game_state(game_state)
+		self.game_state = GameState(ball=ball_state, cars={self.car.index: car_state})
+		self.car.set_game_state(self.game_state)
+		print("whyyy")
